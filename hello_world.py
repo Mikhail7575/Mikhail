e854aa85de171d119b5f3886db86ddcf6f695cfb
Mikhail7575 <ki19452015@gmail.com>
2024-12-16 20:57:03 +0200

    class_work

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,36 @@
-print("hello world")
+a = int(input())
+b = int(input())
+print(a + b)
+
+c = int(input())
+
+d = int(input())
+e = int(input())
+print(d - e)
+
+m = float(int(input()))
+k = float(int(input()))
+print(m * k)
+print(m / k)
+
+lm = input()
+print(lm) 
+
+bl = input()
+rm = input()
+bl == True
+rm == False
+print(bl,rm)
+
+
+jkl = input()
+prk = input()
+print(jkl + prk)
+
+dfg = int(input())
+krg = input()
+print(dfk + int(krg))
+
+
+
+
