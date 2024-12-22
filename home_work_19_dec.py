bfbcbb86df819d1592e241d8d224bad0e715573b
Mikhail7575 <ki19452015@gmail.com>
2024-12-22 18:31:09 +0200

    2_zadacha

--- home_work_19_dec.py
+++ home_work_19_dec.py
@@ -7,3 +7,15 @@ b = [a.lower(), a.upper()]
 random_el = random.choice(b)
 
 print(random_el)
+
+# 2  Длинна слов
+
+word_1 = input('Введите 1-е слово ')
+word_2 = input('Введите 2-е слово ')
+
+if len(word_1) == len(word_2):
+    print('Слова одинаковой длинны')
+elif len(word_1) < len(word_2):
+    print('Первое слово короче чем второе')
+else:
+    print('Первое слово длиннее чем второе')
