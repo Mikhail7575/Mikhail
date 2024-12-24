60eb50c683e2db78a8305c69a6202e5ee4da7160
Mikhail7575 <ki19452015@gmail.com>
2024-12-24 21:21:43 +0200

    chetire_zadachi

--- while_True.py
+++ while_True.py
@@ -1,8 +1,33 @@
 parol = 'Lemmy'
 
 while True:
-    a = input('Введите пароль ')
-    if parol == not(a):
-        print('Пароль не верный')
-    else:
-        print('Вход выполнен') 
+    a = input(' enter password ')
+    if parol == a:
+
+    print('password is true')
+        break
+
+total = 0
+
+#1
+
+for i in range(1,101):
+    total += i
+print(total)
+
+#2
+
+string = input()
+for i in string:
+    print(i)
+
+#3
+number = int(input())
+
+for i in range(1,16):
+    print(i * number)
+
+#4
+n = int(input())
+for i in range(0, n, 2):
+    print(n + i)    
