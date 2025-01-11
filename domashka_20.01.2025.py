6da9349b49d6bc903d30492c3b05dc2cb17fd491
Mikhail7575 <ki19452015@gmail.com>
2025-01-11 18:38:19 +0200

    tri_ingredients

--- domashka_20.01.2025.py
+++ domashka_20.01.2025.py
@@ -1,11 +1,12 @@
 # 1. Дано число n. Создайте список от n до 0.
-'''
+
+import random
 n = int(input())
 new_list = []
 for i in range(n, -1, -1):
     new_list.append(i)
 print(new_list)
-'''
+
 
 # 2. Добавить слово "пушистый" к каждому имени.
 
@@ -16,3 +17,14 @@ for i in dinozaurs:
     b = a + i
     pushistos_dinozaurs.append(b)
 print(pushistos_dinozaurs)
+
+
+# 3. Случайнр выбирает три ингредиента для нового рецепта
+
+
+ingredients = ["сыр", "яйца", "помидоры", "курица", "рыба", "грибы", "лук"]
+new_recept = []
+for i in range(3):
+    b = random.choice(ingredients)
+    new_recept.append(b)
+print(new_recept)
