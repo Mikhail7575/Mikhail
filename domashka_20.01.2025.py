b2b3678f528e51929ac3e27e1748c60fd1fe9d47
Mikhail7575 <ki19452015@gmail.com>
2025-01-11 18:28:26 +0200

    pushistos

--- domashka_20.01.2025.py
+++ domashka_20.01.2025.py
@@ -1,7 +1,18 @@
 # 1. Дано число n. Создайте список от n до 0.
-
+'''
 n = int(input())
 new_list = []
 for i in range(n, -1, -1):
     new_list.append(i)
 print(new_list)
+'''
+
+# 2. Добавить слово "пушистый" к каждому имени.
+
+dinozaurs = ["Ти-рекс", "Трицератопс", "Велоцираптор", "Брахиозавр"]
+pushistos_dinozaurs = []
+a = "Пушистый "
+for i in dinozaurs:
+    b = a + i
+    pushistos_dinozaurs.append(b)
+print(pushistos_dinozaurs)
