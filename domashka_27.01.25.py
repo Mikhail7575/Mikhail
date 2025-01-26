91894d53c2db14854dfc6c3566b9110aaf99465a
Mikhail7575 <ki19452015@gmail.com>
2025-01-26 21:17:18 +0200

    razdelit_na_spiscy

--- domashka_27.01.25.py
+++ domashka_27.01.25.py
@@ -65,4 +65,46 @@ pushistos_dinozaurs = ["Пушистый " + i for i in dinozaurs]
 
 print(pushistos_dinozaurs)
 
+
+
+# Случайнр выбирает три ингредиента для нового рецепта
+import random
+
+ingredients = ["сыр", "яйца", "помидоры", "курица", "рыба", "грибы", "лук"]
+new_recept = []
+new_recept = [random.choice(ingredients) for i in range(3)]
+    
+print(new_recept)
+
+
+
+names = [("Иванов", "Иван"), ("Петров", "Петров"), ("Сидоров", "Сидор")]
+new_names = []
+new_names = [i[::-1] for i in names]
+    
+print(new_names)
+
+
+#  Сгенерируйте пароль из случайного набора символов длинной 8.
+import random
+letters = "abcdefghijklmnopqrwxvzABCDEFJHIJKLNMOPQRWXVZ0123456789"
+password = ""
+password = [random.choice(letters) for i in range(8)]
+
+print(''.join(password))
+
+
+
+#  Разделить числа на два списка четные и не четные
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+even_numbers = []
+not_even_numbers = []
+
+even_numbers = [i for i in numbers if i % 2 == 0]
+print(even_numbers)
+
+not_even_numbers = [i for i in numbers if i % 2 != 0]    
+print(not_even_numbers)
+
 '''
