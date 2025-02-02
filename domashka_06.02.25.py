2bc13a3d3c6688b83c07033cb1eb79f45a1f4ff3
Mikhail7575 <ki19452015@gmail.com>
2025-02-03 00:13:56 +0200

    udalenie

--- domashka_06.02.25.py
+++ domashka_06.02.25.py
@@ -83,4 +83,19 @@ def apply_discount(product):
 discounted_products = map(apply_discount, products)
 print(list(discounted_products))  # Как вывести,чтобы было так же красиво,как products ?
 
+
+
+#6. Удаление товаров с низким рейтингом через filter
+#Фильтруйте товары с рейтингом меньше 4.
+# Задача
+products = [
+    {"name": "Laptop", "rating": 4.5},
+    {"name": "Shirt", "rating": 3.8},
+    {"name": "Phone", "rating": 4.2},
+]
+
+# Используйте filter для удаления товаров с рейтингом < 4
+high_rating_products = filter(lambda x: x["rating"] > 4, products)
+print(list(high_rating_products))
+# Ожидаемый результат: [{"name": "Laptop", "rating": 4.5}, {"name": "Phone", "rating": 4.2}]
 '''
