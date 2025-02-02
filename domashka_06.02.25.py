c06314cb32c5a2fdc9bf65e1a97650105bbcc4bf
Mikhail7575 <ki19452015@gmail.com>
2025-02-02 23:57:33 +0200

    skidka

--- domashka_06.02.25.py
+++ domashka_06.02.25.py
@@ -13,7 +13,7 @@ def add_client():
 
 add_client()
 add_client()
-print(clients)  
+print(clients)
 
 
 
@@ -32,14 +32,14 @@ def apply_discount(discount):
 
 apply_discount(20)  # Цена со скидкой: 80
 print(base_price)   # Ожидаемый результат: 100
-'''
+
 
 
 # 3. Применение налогов через map
 # Список цен товаров. Напишите функцию, которая добавляет к каждому товару налог 20%, и примените её с помощью map.
 # Задача
 
-'''
+
 
 prices = [100, 200, 300, 400]
 
@@ -51,18 +51,36 @@ def apply_tax(price):
 final_prices = list(map(apply_tax, prices))
 print(final_prices)
 
-
-
+#4. Фильтр товаров по цене
+#Есть список цен. Используйте filter, чтобы оставить только товары дороже 150.
+# Задача
 prices = [50, 120, 180, 300, 75]
 
 # Используйте filter, чтобы отобрать только товары дороже 150
+expensive_items = filter(lambda x:x > 150, prices) 
+print(list(expensive_items))       # Ожидаемый результат: [180, 300]
 
 
-def more_150(x):
-    return x > 150
 
 
-expensive_items = filter(more_150, prices)
-print(list(expensive_items))  # Ожидаемый результат: [180, 300]
+# 5. Применение скидки к категории товаров
+# У вас есть список цен и категория, к которой нужно применить скидку. Напишите функцию,
+# которая с помощью map применяет скидку 10% к заданным категориям.
+# Задача
+products = [
+    {"name": "Laptop", "price": 1000, "category": "electronics"},
+    {"name": "Shirt", "price": 50, "category": "clothing"},
+    {"name": "Phone", "price": 600, "category": "electronics"},
+]
+
+
+def apply_discount(product):
+    if product["category"] == "electronics":
+        product["price"] = product["price"] - product["price"] * 10 / 100
+    return product
+
+
+discounted_products = map(apply_discount, products)
+print(list(discounted_products))  # Как вывести,чтобы было так же красиво,как products ?
 
 '''
