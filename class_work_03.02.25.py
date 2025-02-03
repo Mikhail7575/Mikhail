76408cbeca2b8f4f3897b2323bac0fd481879cc6
Mikhail7575 <ki19452015@gmail.com>
2025-02-03 19:50:25 +0200

    animals

---
+++ class_work_03.02.25.py
@@ -0,0 +1,5 @@
+# 1. Создать класс Animal: name, species
+# 2. Создать подкласс Mammal: warm_blood = True; display_info и Bird: can_fly = True; display_info
+# 3. Создать класс Zoo: animals = []; add_animal, show_all_animals
+
+class animal:
