6378ffa35edca94325f24d2adbbaa7881efc6e75
Mikhail7575 <ki19452015@gmail.com>
2025-02-03 20:29:59 +0200

    zoo

--- class_work_03.02.25.py
+++ class_work_03.02.25.py
@@ -3,3 +3,22 @@
 # 3. Создать класс Zoo: animals = []; add_animal, show_all_animals
 
 class animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+
+    def display_info_(self):
+        print(f"{self.name} - {self.species}")
+
+
+class Mammal(animal):
+    def __init__(self, name, species):
+		super().__init__(name, species)
+        self.warm_blood = warm_blood    
+    
+	def display_info_(self):
+         print(f"{self.name} - {self.species} warm_blood = {self.warm_blood} ")
+         
+class Zoo(animal):
+     def __init__(self, name, species):
+		  super().__init__(name, species)		 
