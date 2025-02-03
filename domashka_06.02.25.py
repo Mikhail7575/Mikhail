742ee741c6c511d9953b78c815a82e144d9b583e
Mikhail7575 <ki19452015@gmail.com>
2025-02-03 12:33:25 +0200

    summa

--- domashka_06.02.25.py
+++ domashka_06.02.25.py
@@ -170,4 +170,17 @@ products = [
 phones = filter(lambda x: 'Phone' in x['name'], products)
 print(list(phones))
 
+
+
+# 11. Сумма произвольного количества чисел (*args)
+# Напишите функцию, которая принимает произвольное количество чисел и возвращает их сумму.
+# Задача
+
+
+def sum_numbers(*args):
+    return sum(args)                # Верните сумму всех переданных чисел
+
+
+result = sum_numbers(1, 2, 3, 4, 5)
+print(result)                       # Ожидаемый результат: 15
 '''
